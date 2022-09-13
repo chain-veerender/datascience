f80acaef0e47cc79aa70607cdb4b5a7eb682f061
veerender <bvr.nani@gmail.com>
2022-09-13 17:19:43 +0530

    Delete 2017-feb directory

--- 2017-feb/22.spark-basics/pyspark-test1.x.py
+++
@@ -1,8 +0,0 @@
-from pyspark import SparkContext, SQLContext
-
-sc = SparkContext("local", "Test")
-print(sc)
-
-sqlContext = SQLContext(sc)
-sqlContext
-
--- 2017-feb/22.spark-basics/pyspark-test2.x.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql import SparkSession
-
-sparkSession1 = SparkSession.builder.master("local").appName("Test2").getOrCreate()
-     
-sparkSession2 = SparkSession.builder.master("local").appName("Test2").enableHiveSupport().getOrCreate()
-
-
