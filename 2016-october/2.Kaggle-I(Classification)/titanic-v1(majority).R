497e84d40e001765da7104b123ec812c3f648e17
veerender <bvr.nani@gmail.com>
2022-09-13 17:18:34 +0530

    Delete 2016-october directory

--- 2016-october/2.Kaggle-I(Classification)/titanic-v1(majority).R
+++
@@ -1,5 +0,0 @@
-titanic_test = read.csv("C:/Users/Algorithmica/Downloads/test.csv")
-class(titanic_test)
-dim(titanic_test)
-titanic_test$Survived=0
-write.csv(titanic_test[,c("PassengerId","Survived")],"C:/Users/Algorithmica/Downloads/submission.csv", row.names = F)
--- 2016-october/2.Kaggle-I(Classification)/titanic-v2(random).R
+++
@@ -1,4 +0,0 @@
-titanic_test = read.csv("C:/Users/Algorithmica/Downloads/test.csv")
-dim(titanic_test)
-titanic_test$Survived=sample(c(0,1),418,replace=T)
-write.csv(titanic_test[,c("PassengerId","Survived")],"C:/Users/Algorithmica/Downloads/submission.csv", row.names = F)
