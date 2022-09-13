0ed37e89868f2577b3bd37cb64d889458d7059a8
veerender <bvr.nani@gmail.com>
2022-09-13 17:21:08 +0530

    Delete 2018-jan directory

--- 2018-jan/1.python/3.tuple.py
+++
@@ -1,6 +0,0 @@
-tuple1 = (1,2,3,4,5)
-print(tuple1)
-
-print(tuple1[0:3])
-print(tuple1[3:5])
-
--- 2018-jan/1.python/8.main.py
+++
@@ -1,7 +0,0 @@
-import sklearn
-
-print(sklearn.__version__)
-print(__name__)
-
-if __name__ == '__main__':
-    print("hello")
