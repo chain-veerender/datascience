52f1015f29ade16c312e54bff3df140ef0443ae6
veerender <bvr.nani@gmail.com>
2022-09-13 17:18:54 +0530

    Delete 2016 directory

--- 2016/4-EDA numerical/multivariate-stats-cor-cov.R
+++
@@ -1,9 +0,0 @@
-setwd("E:/data analytics/datasets")
-
-winedata = read.csv("wine.data", header = TRUE)
-dim(winedata)
-str(winedata)
-head(winedata)
-
-cov_matrix = cov(winedata)
-cor_matrix = cor(winedata)
